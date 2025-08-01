e47f81e25d6a7c6cdd5054ac4bd0d382d0ccd9f8
harshgajera101 <xsport102@gmail.com>
2025-08-01 19:47:26 +0530

    Added InfoAgenda Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/AboutPage/About';
 import Home from './components/HomePage/Home';
 import GalleryPage from './components/GalleryPage/GalleryPage';
+import InfoAgenda from './components/InfoAgenda/InfoAgenda';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/InfoAgenda" element={<InfoAgenda />} />
         </Routes>
     </Router>
     </>
