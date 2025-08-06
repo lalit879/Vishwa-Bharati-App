b5b01d2e1ae55343dc59619dbd6e8685afa209a4
harshgajera101 <xsport102@gmail.com>
2025-08-06 18:32:20 +0530

    minor change in nav & agendacard

--- src/components/InfoAgenda/AgendaCard.jsx
+++ src/components/InfoAgenda/AgendaCard.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AgendaCard = ({ icon: Icon, iconColor, title, line1, line2 }) => {
   return (
     <div className="border-2 border-blue-300 rounded-lg py-6 px-4 text-center">
-      <Icon className={`mx-auto text-3xl mb-2 ${iconColor}`} />
+      <Icon className={`mx-auto text-3xl mb-4 ${iconColor}`} />
       <p className="font-semibold mb-4">{title}</p>
       <p className="mt-1">{line1}</p>
       <p className="text-sm text-gray-500">{line2}</p>
